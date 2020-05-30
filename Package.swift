f6126d622ebbf1f360aa522e2f699d509bf8da42
Matis Schotte <dm26f1cab8aa26@ungeord.net>
2020-05-30 16:25:18 +0900

    Add platform constraints  (iOS 10+, macOS 10.12+, tvOS 10+, watchOS 3+)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "UNTracingActivity",
+	platforms: [
+		.iOS(.v10),
+		.macOS(.v10_12),
+		.tvOS(.v10),
+		.watchOS(.v3),
+	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
