92c0206e069527bf907d0232d8f1256a1b00b993
Matis Schotte <dm26f1cab8aa26@ungeord.net>
2020-05-30 16:15:42 +0900

    Add UNTracingActivityTests

--- Tests/UNTracingActivityTests/UNTracingActivityTests.swift
+++ Tests/UNTracingActivityTests/UNTracingActivityTests.swift
@@ -2,14 +2,70 @@ import XCTest
 @testable import UNTracingActivity
 
 final class UNTracingActivityTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UNTracingActivity().text, "Hello, World!")
-    }
+	func testInitiate() throws {
+		let blockExecutedInActivity = expectation(description: "testInitiate.blockExecutedInActivity")
+		let blockExecuted = expectation(description: "testInitiateblockExecuted")
+		
+		_ = UNTracingActivity.initiate("ActivitySuccess") {
+			blockExecutedInActivity.fulfill()
+		}
+		
+		// when dso is nil the activity cannot be created
+		_ = UNTracingActivity.initiate("", dso: nil) {
+			blockExecuted.fulfill()
+		}
+		
+		waitForExpectations(timeout: 1)
+	}
+	
+	func testFailableInit() throws {
+		XCTAssertNotNil(UNTracingActivity("ActivitySuccess"))
+		
+		XCTAssertNil(UNTracingActivity("", dso: nil))
+	}
+	
+	func testStaticApplyPositive() throws {
+		let blockExecutedInActivity = expectation(description: "testApply.blockExecutedInActivity")
+		
+		let activitySuccess = UNTracingActivity("ActivitySuccess")
+		let result = UNTracingActivity.apply(activitySuccess) {
+			blockExecutedInActivity.fulfill()
+		}
+		XCTAssertTrue(result)
+		
+		waitForExpectations(timeout: 1)
+	}
+	
+	func testStaticApplyNegative() throws {
+		let blockExecuted = expectation(description: "testStaticApply.blockExecuted")
+		
+		// when dso is nil the activity cannot be created
+		let activityFailure = UNTracingActivity("", dso: nil)
+		let result = UNTracingActivity.apply(activityFailure) {
+			blockExecuted.fulfill()
+		}
+		XCTAssertFalse(result)
+		
+		waitForExpectations(timeout: 1)
+	}
+	
+	func testEnterLeaveScope() throws {
+		var scopeChanged = false
+		
+		let activity = UNTracingActivity("TestActivity")
+		var scope = activity?.enter() {
+			didSet { scopeChanged.toggle() }
+		}
+		scope?.leave()
+		
+		XCTAssertTrue(scopeChanged)
+	}
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testInitiate", testInitiate),
+		("testFailableInit", testFailableInit),
+		("testStaticApplyPositive", testStaticApplyPositive),
+		("testStaticApplyNegative", testStaticApplyNegative),
+		("testEnterLeaveScope", testEnterLeaveScope),
     ]
 }
